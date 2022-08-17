1196fb5255d3be2e95a59b48dc8c1c084b6b403b
sergeypil <serejas_mail@mail.ru>
2022-08-17 16:07:05 +0600

    Add spring cloud gateway

--- elasticsearch-query-web-client/src/main/java/net/serg/elasticsearch/query/web/client/config/WebClientConfig.java
+++ elasticsearch-query-web-client/src/main/java/net/serg/elasticsearch/query/web/client/config/WebClientConfig.java
@@ -4,18 +4,15 @@ import io.netty.channel.ChannelOption;
 import io.netty.handler.timeout.ReadTimeoutHandler;
 import io.netty.handler.timeout.WriteTimeoutHandler;
 import net.serg.config.ElasticsearchQueryWebClientConfigData;
-import net.serg.config.UserConfigData;
 import org.apache.http.HttpHeaders;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
-import org.springframework.web.reactive.function.client.ExchangeFilterFunctions;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
 import reactor.netty.tcp.TcpClient;
@@ -25,13 +22,13 @@ import java.util.concurrent.TimeUnit;
 @Configuration
 public class WebClientConfig {
 
-    private final ElasticsearchQueryWebClientConfigData.WebClient elasticQueryWebClientConfigData;
+    private final ElasticsearchQueryWebClientConfigData.WebClient elasticsearchQueryWebClientConfigData;
 
     @Value("${security.default-client-registration-id}")
     private String defaultClientRegistrationId;
 
     public WebClientConfig(ElasticsearchQueryWebClientConfigData webClientConfigData) {
-        this.elasticQueryWebClientConfigData = webClientConfigData.getWebClient();
+        this.elasticsearchQueryWebClientConfigData = webClientConfigData.getWebClient();
     }
 
     @LoadBalanced
@@ -49,26 +46,26 @@ public class WebClientConfig {
         oauth2.setDefaultClientRegistrationId(defaultClientRegistrationId);
 
         return WebClient.builder()
-                .baseUrl(elasticQueryWebClientConfigData.getBaseUrl())
-                .defaultHeader(HttpHeaders.CONTENT_TYPE, elasticQueryWebClientConfigData.getContentType())
-                .defaultHeader(HttpHeaders.ACCEPT, elasticQueryWebClientConfigData.getAcceptType())
+                .baseUrl(elasticsearchQueryWebClientConfigData.getBaseUrl())
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, elasticsearchQueryWebClientConfigData.getContentType())
+                .defaultHeader(HttpHeaders.ACCEPT, elasticsearchQueryWebClientConfigData.getAcceptType())
                 .clientConnector(new ReactorClientHttpConnector(HttpClient.from(getTcpClient())))
                 .apply(oauth2.oauth2Configuration())
                 .codecs(clientCodecConfigurer ->
                         clientCodecConfigurer
                                 .defaultCodecs()
-                                .maxInMemorySize(elasticQueryWebClientConfigData.getMaxInMemorySize()));
+                                .maxInMemorySize(elasticsearchQueryWebClientConfigData.getMaxInMemorySize()));
     }
 
     private TcpClient getTcpClient() {
         return TcpClient.create()
-                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, elasticQueryWebClientConfigData.getConnectTimeoutMs())
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, elasticsearchQueryWebClientConfigData.getConnectTimeoutMs())
                 .doOnConnected(connection -> {
                     connection.addHandlerLast(
-                            new ReadTimeoutHandler(elasticQueryWebClientConfigData.getReadTimeoutMs(),
+                            new ReadTimeoutHandler(elasticsearchQueryWebClientConfigData.getReadTimeoutMs(),
                                     TimeUnit.MILLISECONDS));
                     connection.addHandlerLast(
-                            new WriteTimeoutHandler(elasticQueryWebClientConfigData.getWriteTimeoutMs(),
+                            new WriteTimeoutHandler(elasticsearchQueryWebClientConfigData.getWriteTimeoutMs(),
                                     TimeUnit.MILLISECONDS));
                 });
     }
---
+++ gateway-service/src/main/java/net/serg/gateway/service/model/QueryServiceFallbackModel.java
@@ -0,0 +1,14 @@
+package net.serg.gateway.service.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class QueryServiceFallbackModel {
+    private String fallbackMessage;
+}
